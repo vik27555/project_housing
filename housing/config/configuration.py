835f9f3b2ce1036f5067a1a1381627a02e35b74c
vik27555 <101319147+vik27555@users.noreply.github.com>
2022-07-04 15:38:07 +0530

    update

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -1,4 +1,5 @@
 
+from msilib import schema
 from housing.entity.config_entity import DataIngestionConfig, DataTransformationConfig,DataValidationConfig,   \
 ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig,TrainingPipelineConfig
 from housing.util.util import read_yaml_file
@@ -24,7 +25,6 @@ class Configuartion:
 
     def get_data_ingestion_config(self) ->DataIngestionConfig:
         try:
-            raise Exception("Testing exception")
             artifact_dir = self.training_pipeline_config.artifact_dir
             data_ingestion_artifact_dir=os.path.join(
                 artifact_dir,
@@ -70,10 +70,34 @@ class Configuartion:
 
     def get_data_validation_config(self) -> DataValidationConfig:
         try:
-            
-            schema_file_path = None
+            artifact_dir = self.training_pipeline_config.artifact_dir
+
+            data_validation_artifact_dir=os.path.join(
+                artifact_dir,
+                DATA_VALIDATION_ARTIFACT_DIR_NAME,
+                self.time_stamp
+            )
+            data_validation_config = self.config_info[DATA_VALIDATION_CONFIG_KEY]
+
+ 
+            schema_file_path = os.path.join(ROOT_DIR,
+            data_validation_config[DATA_VALIDATION_SCHEMA_DIR_KEY],
+            data_validation_config[DATA_VALIDATION_SCHEMA_FILE_NAME_KEY]
+            )
+
+            report_file_path = os.path.join(data_validation_artifact_dir,
+            data_validation_config[DATA_VALIDATION_REPORT_FILE_NAME_KEY])
+
+            report_page_file_path = os.path.join(data_validation_artifact_dir,
+            data_validation_config[DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY]
+            )
+
+
+
             data_validation_config = DataValidationConfig(
-                schema_file_path=schema_file_path
+                schema_file_path=schema_file_path,
+                report_file_path=report_file_path,
+                report_page_file_path=report_page_file_path,
             )
             return data_validation_config
         except Exception as e:
@@ -103,8 +127,7 @@ class Configuartion:
             logging.info(f"Training pipleine config: {training_pipeline_config}")
             return training_pipeline_config
         except Exception as e:
-            raise HousingException(e,sys) from e
-        
+            raise HousingException(e,sys) from e        
 
 
 
